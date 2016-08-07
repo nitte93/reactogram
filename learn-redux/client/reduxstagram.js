8f1c2a4ed7b0549795bfc37d96643f6b807df750
Nitesh <nitesh.tiwari2011@vitalum.ac.in>
2016-08-07 19:41:01 +0530

    reactogram store and reducers

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,35 @@
 // let's go!
+import React from 'react';
+import {render} from 'react-dom';
+
+//import css
+import css from './styles/style.styl';
+
+//import components
+import App from './components/App.js'
+import Single from './components/Single.js'
+import PhotoGrid from './components/PhotoGrid.js'
+
+//import react router deps
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+//sentry
+// import Raven from 'raven-js';
+// import { sentry_url } from './data/config';
+//
+// Raven.config(sentry_url).install();
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path='/' component={App}>
+          <IndexRoute component={PhotoGrid}></IndexRoute>
+          <Route path="/view/:postId" component={Single}>
+          </Route>
+      </Route>
+    </Router>
+  </Provider>
+)
+render(router, document.getElementById('root'));
