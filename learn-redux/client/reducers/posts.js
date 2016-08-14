5d943088e3e9b7af244a6744131d41872b12c20d
Nitesh <nitesh.tiwari2011@vitalum.ac.in>
2016-08-15 03:15:18 +0530

    bindActionCreator, reactCloneElements

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,21 @@
+//a recuder takes in two things
+//1) action
+//2) copy of current state
+
+function posts(state = [], action){
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      const i= action.index;
+      return [
+        ...state.slice(0,i),
+        {...state[i], likes:state[i].likes +1},
+        ...state.slice(i+1)
+      ]
+    default:
+
+  }
+  return state
+}
+
+
+export default posts
